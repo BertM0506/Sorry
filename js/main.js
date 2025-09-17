d49179775a5aa81ec131b3eb407cf4a498e3f8cc
BertM0506 <tberkat13@gmail.com>
2025-09-17 23:51:49 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('SELAMAT MALAM RAFAELAAA ').split('')
+    const titles = ('SELAMAT MALAM RAFAELAAAAA ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
