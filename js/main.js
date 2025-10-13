5f8671c24fb8d71db9ba05e2e5e89836952b9571
BertM0506 <tberkat13@gmail.com>
2025-10-13 23:23:19 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('SELAMAT MALAM RAFAELAAAAA ').split('')
+    const titles = ('SELAMAT MALAM REGIA CANTIKK, SEMANGAT UTSNYAAAA YAA ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
